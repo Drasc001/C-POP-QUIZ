a63328669081926069e82ef22b1464873ac6a3b4
Drasc001 <dtr2170@email.vccs.edu>
2025-02-07 17:34:46 -0500

    Beginning Of Challenge1

---
+++ Challenge1.cpp
@@ -0,0 +1,10 @@
+{
+	char first, middle, last;
+	first = ‘S’;
+	middle = ‘K’;
+	last = ‘S’;
+	cout << first << middle << last;
+	return 0;
+}
+Replace the code in the main function with the code for this program.Compile and run the
+program.Verify that it is producing the correct results.
